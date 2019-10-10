440fbacb05f9aba05095e207cb1eff08f151554b
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-10-10 20:40:17 -0300

    refectory

--- src/test/java/com/jullierme/restfuldemo/model/PersonUTest.java
+++ src/test/java/com/jullierme/restfuldemo/model/PersonUTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@DisplayName("Test suite of the class Person")
+@DisplayName("Person class unit test suite")
 public class PersonUTest {
 
     @Test
